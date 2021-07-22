fa345b3523b937f9f682ab3ebf62fb0f2d6379cd
Matt Jarrett <16245634+cujarrett@users.noreply.github.com>
2021-07-21 19:42:25 -0500

    fix: migrate to ava for tests (#10)

--- test/integration/github.js
+++ test/integration/github.js
@@ -1,20 +1,13 @@
-const test = require("tape-async")
-// eslint-disable-next-line max-len
+const test = require("ava")
 const { getContributionYears, getRawContributionData } = require("../../src/integrations/github.js")
 
 test("Integration - GitHub", async (assert) => {
-  assert.plan(5)
   let contributionYears = await getContributionYears("cujarrett")
-  // eslint-disable-next-line max-len
-  assert.equal(contributionYears[contributionYears.length - 1], "2015", "getContributionYears returns first year of contributions for author verified")
+  assert.is(contributionYears[contributionYears.length - 1], "2015")
   contributionYears = await getContributionYears("jjshfvjsdvjksjncvjsbncjkslHV")
-  // eslint-disable-next-line max-len
-  assert.equal(contributionYears.length, 0, "getContributionYears returns first year of contributions for author verified")
+  assert.is(contributionYears.length, 0)
   const contributions = await getRawContributionData("cujarrett", [2015])
-  // eslint-disable-next-line max-len
-  assert.equal(contributions.length, 365, "getRawContributionData returns a full year of results verified")
-  // eslint-disable-next-line max-len
-  assert.equal(contributions[0].count, 0, "getRawContributionData returns expected contributions for a given day verified")
-  // eslint-disable-next-line max-len
-  assert.equal(contributions[343].count, 1, "getRawContributionData returns expected contributions for a given day verified")
+  assert.is(contributions.length, 365)
+  assert.is(contributions[0].count, 0)
+  assert.is(contributions[343].count, 1)
 })
